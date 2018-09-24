45577154ad497787ced4f78c323c9f4ed7a9b169
Jordan Senken <jordansenken@Jordans-MBP.attlocal.net>
2018-09-23 21:55:37 -0400

    added summary

--- server.js
+++ server.js
@@ -12,16 +12,18 @@ app.use(logger("dev"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/newScrape");
+mongoose.connect("mongodb://localhost/newScrape", { useNewUrlParser: true });
 
 app.get("/scrape", function(req, res) {
     axios.get("http://www.espn.com/fantasy/football/").then(function(response) {
     var $ = cheerio.load(response.data);
-    $("a.realStory").each(function(i,element) {
+    $("div.item-info-wrap").each(function(i,element) {
         var result = {};
-        result.title = $(this)
+        result.title = $(this).children("h1").children("a")
         .text();
-        result.link = $(this)
+        result.summary = $(this).children("p").text();
+        // console.log(result.summary);
+        result.link = $(this).children("h1").children("a")
         .attr("href");
         db.Article.create(result)
         .then(function(dbArticle) {
@@ -38,15 +40,19 @@ app.get("/scrape", function(req, res) {
 app.get("/articles", function(req, res) {
     db.Article.find({}).then(function(dbArticle){
       res.json(dbArticle)
+    }).catch(function(err) {
+      res.json(err);
     });
   });
 
   app.get("/articles/:id", function(req, res) {
     db.Article.findOne({_id: req.params.id})
-      .populate("notes")
+      .populate("note")
       .then(function(dbArticle){
         res.json(dbArticle)
-      })
+      }).catch(function(err){
+        res.json(err);
+      });
   });
 
   app.post("/articles/:id", function(req, res) {
@@ -55,6 +61,8 @@ app.get("/articles", function(req, res) {
         return db.Article.findOneAndUpdate({_id: req.params.id}, {note: dbNote._id}, {new: true});
       }).then(function(dbArticle) {
         res.json(dbArticle)
+      }).catch(function(err){
+        res.json(err);
       })
   });
   
